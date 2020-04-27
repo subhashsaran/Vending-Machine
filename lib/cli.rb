e51021ecb11e359f29ec07971caa175fa8f12b25
Daniel Lockhart <daniel@lockyy.com>
2020-04-27 16:49:01 +0100

    Add the ability to insert coin in CLI
    I initially built this so that the the user_input case statement in CLI.run had a number of separate options for each different coin, so it was similar to `when ‘insert £1’, ‘insert £2’` (though dynamically generated).
    
    The user input then got passed through into insert_coin where it was split apart and then the option was used as it currently is.
    
    This felt a little off however and so I tried out swapping over to splitting the command and option apart prior to triggering the case statement. This lead to a much nicer system that will provide a smoother implementation of the purchasing flow later down the line.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Dir[File.join(__dir__, '**', '*.rb')].sort.each { |file| require file }
 require 'money'
 I18n.enforce_available_locales = false
@@ -24,15 +26,19 @@ class CLI
     output_cursor
 
     user_input = ''
-
     while user_input != EXIT_OPTION
       user_input = gets.chomp
 
-      case user_input
+      args = user_input.split(' ')
+      command = args[0]
+      # Currently the CLI ignores any options after the first
+      option = args[1]
+
+      case command
       when BALANCE_OPTION
         output_balance
       when INSERT_COIN_OPTION
-        output_unimplemented
+        insert_coin(input: option)
       when HELP_OPTION
         output_options
       when CLEAR_OPTION
@@ -49,6 +55,7 @@ class CLI
 
   private
 
+  # Vending Machine set up
   attr_reader :vending_machine
 
   def initial_products
@@ -58,6 +65,8 @@ class CLI
   def initial_change
     []
   end
+
+  # Interface output
 
   def clear_screen
     system 'clear'
@@ -73,38 +82,63 @@ class CLI
     puts 'Welcome to Vending Machine'
   end
 
-  def output_options
-    options = {
-      BALANCE_OPTION     => 'Output Balance',
-      INSERT_COIN_OPTION => 'Insert Coin',
-      HELP_OPTION        => 'Display these options',
-      CLEAR_OPTION       => 'Clear history',
-      EXIT_OPTION        => 'Close CLI'
-    }
-    longest_option_length = options.keys.map(&:length).max
-
-    puts 'Available Options'
-    puts '================='
-
-    options.each do |option, description|
-      # Using ljust here to neatly format the options and descriptions
-      # +1 to account for the colon
-      option_string = "#{option}:".ljust(longest_option_length + 1)
-      puts "#{option_string} #{description}"
-    end
-  end
-
   def output_cursor
     print '> '
   end
 
   def output_invalid_input
-    puts 'Invalid Input'
+    output_error(error: 'Invalid Input')
   end
 
   def output_unimplemented
-    puts 'Unimplemented'
+    output_error(error: 'Unimplemented')
   end
+
+  def output_error(error:)
+    puts "ERROR: #{error}"
+  end
+
+  # Options output
+
+  def options
+    {
+      BALANCE_OPTION => 'Output Balance',
+      "#{INSERT_COIN_OPTION} <x>" => "Insert Coin (options: #{insert_coin_custom_input_examples})",
+      HELP_OPTION => 'Display these options',
+      CLEAR_OPTION => 'Clear history',
+      EXIT_OPTION => 'Close CLI'
+    }
+  end
+
+  def output_options
+    puts 'Available Options'
+    puts '================='
+
+    options.each do |option, description|
+      output_option(option: option, description: description)
+    end
+  end
+
+  def output_option(option:, description:)
+    # Using ljust here to neatly format the options and descriptions
+    # +1 to account for the colon on the longest option
+    option_string = "#{option}:".ljust(longest_option_length + 1)
+    puts "#{option_string} #{description}"
+  end
+
+  def longest_option_length
+    options.keys.map(&:length).max
+  end
+
+  def valid_coin_names
+    Coin::VALID_COIN_NAMES
+  end
+
+  def insert_coin_custom_input_examples
+    valid_coin_names.join(', ')
+  end
+
+  # Input Handling
 
   def output_balance
     puts "Current Balance: #{format_currency(vending_machine.balance)}"
@@ -112,5 +146,16 @@ class CLI
 
   def format_currency(value)
     Money.new(value, 'GBP').format
+  end
+
+  def insert_coin(input:)
+    coin = Coin.from_string(string: input)
+
+    if vending_machine.insert_coin(coin: coin)
+      puts 'Coin Inserted'
+      output_balance
+    else
+      output_error(error: "Invalid argument #{input}")
+    end
   end
 end
--- lib/coin.rb
+++ lib/coin.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 
 class Coin
-  VALID_COINS = [1, 2, 5, 10, 20, 50, 100, 200].freeze
+  COINS = {
+    '£2'  => 200,
+    '£1'  => 100,
+    '50p' => 50,
+    '20p' => 20,
+    '10p' => 10,
+    '5p'  => 5,
+    '2p'  => 2,
+    '1p'  => 1
+  }
+  VALID_COIN_NAMES = COINS.keys.freeze
+  VALID_COIN_VALUES = COINS.values.freeze
 
   class << self
     def valid_coins
-      Coin::VALID_COINS
+      Coin::VALID_COIN_VALUES
+    end
+
+    def from_string(string:)
+      value = Coin::COINS[string]
+      return Coin.new(value: value) unless value.nil?
+
+      invalid_coin
+    end
+
+    def invalid_coin
+      Coin.new(value: -1)
     end
   end
 
