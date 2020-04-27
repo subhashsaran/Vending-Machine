ac646f6b44e0be1a539eb2e8b8abac046f61fe94
Daniel Lockhart <daniel@lockyy.com>
2020-04-27 15:54:26 +0100

    Add system for inserting coins into vending machine
    This’ll take in coins and store them in an array. There’s no way to see which coins you’ve inserted (like a real vending machine) but you can see your balance which just returns a sum of the inserted coins value. To the outside they all go into one value but internally we are tracking the coins returned so they can be added to the change pile for future purchases, returned during the current transaction, or returned to the user if they cancel their transaction (if we get time to implementing that).

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,7 +2,25 @@ class VendingMachine
   def initialize(products: [], change: [])
     @products = products
     @change = change
+
+    @inserted_coins = []
   end
 
   attr_reader :products
+
+  def balance
+    inserted_coins.map(&:value).sum
+  end
+
+  def insert_coin(coin:)
+    return false unless coin.is_a?(Coin)
+    return false unless coin.valid?
+
+    inserted_coins << coin
+    true
+  end
+
+  private
+
+  attr_reader :inserted_coins
 end
--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -1,19 +1,53 @@
 RSpec.describe VendingMachine do
-  let(:vending_machine) { VendingMachine.new(products: products, change: change) }
+  let(:vending_machine) { VendingMachine.new(products: initial_products, change: initial_change) }
 
-  let(:products) { [] }
-  let(:change) { [] }
+  let(:initial_products) { [] }
+  let(:initial_change) { [] }
 
   describe '.products' do
     subject { vending_machine.products }
 
-    let(:products) {
+    let(:initial_products) {
       [
         Product.new(name: 'Pepsi', price: 65),
         Product.new(name: 'Banana', price: 50)
       ]
     }
 
-    it { should be products }
+    it { should be initial_products }
+  end
+
+  describe '.insert_coin' do
+    subject { vending_machine.insert_coin(coin: coin) }
+
+    context 'with a valid coin' do
+      let(:coin) { Coin.new(value: 20) }
+
+      it 'adds the coin to the users balance' do
+        expect { subject }.to change(vending_machine, :balance).from(0).to(coin.value)
+      end
+
+      it { should be true }
+    end
+
+    context 'with an invalid coin' do
+      let(:coin) { Coin.new(value: 21) }
+
+      it 'does not add the coin to the users balance' do
+        expect { subject }.not_to change(vending_machine, :balance)
+      end
+
+      it { should be false }
+    end
+
+    context 'with a non-coin' do
+      let(:coin) { 21 }
+
+      it 'does not add the coin to the users balance' do
+        expect { subject }.not_to change(vending_machine, :balance)
+      end
+
+      it { should be false }
+    end
   end
 end
